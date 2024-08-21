e00a4b6e439c1682c0ddf2bdc5e2176ec959ed86
Przemo <przemo06133@gmail.com>
2024-08-21 22:13:10 +0200

    Add error handling for camera visibility

--- greengrass/camera-component/src/CameraClient.py
+++ greengrass/camera-component/src/CameraClient.py
@@ -8,10 +8,17 @@ class CameraClient:
         self._init_client()
 
     def _init_client(self):
-        self.picam2 = Picamera2()
-        preview_config = self.picam2.create_preview_configuration(main={"format": "XRGB8888", "size": (1920, 1080)})
-        self.picam2.configure(preview_config)
-        self.picam2.start()
+        try:
+            self.picam2 = Picamera2()
+            preview_config = self.picam2.create_preview_configuration(main={"format": "XRGB8888", "size": (1920, 1080)})
+            self.picam2.configure(preview_config)
+            self.picam2.start()
+        except IndexError as e:
+            raise RuntimeError(
+                "No camera devices found or accessible. Please check the device permissions and connections.") from e
+        except PermissionError as e:
+            raise RuntimeError(
+                "Permission denied while accessing camera devices. Ensure the process has appropriate permissions.") from e
 
     def capture_snapshot(self):
         self.picam2.capture_file(f"/greengrass/files/camera/snapshot_{int(time.time())}.jpg")
