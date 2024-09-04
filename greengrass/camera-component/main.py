8c98fe52454682eaf3a1a1ad4cc086b281a50d30
Przemo <przemo06133@gmail.com>
2024-09-04 22:58:22 +0200

    camera: Refactor mqtt listener

--- greengrass/camera-component/main.py
+++ greengrass/camera-component/main.py
@@ -1,59 +1,18 @@
 import sys
 from src.CameraClient import CameraClient
-import json
-import time
-import paho.mqtt.client as mqtt
-
-# MQTT Broker details
-broker = "localhost"  # Use "localhost" if using a local broker
-port = 1883
-topic = "camera/snapshot"
-
-
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to MQTT Broker!")
-        client.subscribe(topic)
-        print(f"Subscribed to topic: {topic}")
-    else:
-        print("Failed to connect, return code %d\n", rc)
-
-
-def on_message(client, userdata, msg):
-    print(f"Message received: {msg.payload.decode()} on topic {msg.topic}")
-    try:
-        # Assuming that any message received on this topic triggers a snapshot
-        camera_client.capture_snapshot()
-        print("Snapshot created")
-    except Exception as e:
-        print("Error capturing snapshot:", e)
+from src.MqttListener import MqttListener
 
 
 def main():
     try:
-        print("Start camera component")
         args = sys.argv[1:]
+        print("Start camera component")
 
-        global camera_client
         camera_client = CameraClient()
-        print("Camera client connected")
-
-        # MQTT Client setup
-        mqtt_client = mqtt.Client()
-        mqtt_client.on_connect = on_connect
-        mqtt_client.on_message = on_message
-
-        # Connect to the MQTT broker
-        mqtt_client.connect(broker, port)
-
-        # Start the MQTT client loop
-        mqtt_client.loop_start()
+        mqtt_client = MqttListener(camera_client)
 
         print("Waiting for messages...")
-
-        # Keep the main thread alive to listen for messages indefinitely
-        while True:
-            time.sleep(1)
+        mqtt_client.loop_connection()
 
     except Exception as e:
         print("Error in main:", e)
